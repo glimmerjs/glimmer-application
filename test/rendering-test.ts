54c430ded000496e73708acf7d0d71b4a6494a75
Chad Hietala <chadhietala@gmail.com>
2017-04-09 10:19:22 -0700

    Tests pass

--- test/rendering-test.ts
+++ test/rendering-test.ts
@@ -6,17 +6,20 @@ const { module, test } = QUnit;
 module('Rendering')
 
 test('Does not schedule revalidation if we are current', (assert) => {
-
+  let done = assert.async();
   let updates = 0;
+  let data = [[1], [2]];
 
   class MyThing extends TestComponent {
     grid: Array<Array<number>>;
 
     constructor() {
-      super()
-      this.grid = [[1], [2]];
+      super();
+      this.grid = data;
     }
+  }
 
+  class GridItem extends TestComponent {
     didUpdate() {
       updates++;
     }
@@ -33,12 +36,18 @@ test('Does not schedule revalidation if we are current', (assert) => {
     {{/each}}
     </div>
   `)
-  .component('grid-item', MyThing)
+  .template('grid-item', '<h1>Hello</h1>')
+  .component('grid-item', GridItem)
+  .component('my-thing', MyThing)
   .boot();
 
   assert.equal(updates, 0, 'Does not update on initial render');
 
-  app.rerender();
+  setTimeout(() => {
+    assert.equal(updates, 0, 'Does not update on initial render');
+    app.rerender();
+    assert.equal(updates, 0, 'Does not update on idempotent re-render');
+    done();
+  }, 1000);
 
-  assert.equal(updates, 0, 'Does not update on idempotent re-render');
 });
